1c06fa14ff68fee39a65ff67d509dcd63150fe03
Rahul <rahulmathur87@gmail.com>
2025-12-29 19:30:47 +0530

    added location permissions on app launch and location printing

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +8,65 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      printLocation();
+    });
+  }
+
+  // Check Location Service & Permission
+  Future<bool> checkPermission() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      await Geolocator.openLocationSettings();
+      return false;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      await Geolocator.openAppSettings();
+      return false;
+    }
+
+    return permission == LocationPermission.whileInUse ||
+        permission == LocationPermission.always;
+  }
+
+  // Get the Current Location
+  Future<Position?> getCurrentLocation() async {
+    bool hasPermission = await checkPermission();
+    if (!hasPermission) return null;
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 0,
+    );
+    return await Geolocator.getCurrentPosition(
+      locationSettings: locationSettings,
+    );
+  }
+
+  Future<void> printLocation() async {
+    Position? position = await getCurrentLocation();
+    if (position != null) {
+      print("Latitude: ${position.latitude}");
+      print("Longitude: ${position.longitude}");
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             //Get the current location
+            printLocation();
           },
           child: Text('Get Location'),
         ),
