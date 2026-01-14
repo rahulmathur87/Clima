54a49e4e2ad949f890f28e002a9ad8d5e784f1fb
Rahul <rahulmathur87@gmail.com>
2026-01-14 13:43:58 +0530

    connected openweathermap api and hid api key using environment variables.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() { runApp(MyApp());}
+Future<void> main() async {
+  await dotenv.load(fileName: "secrets.env");
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,11 +16,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocation();
   }
 
+  final apiKey = dotenv.env['API_KEY'];
+  late double latitude;
+  late double longitude;
+
+
   Future<void> getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+    getData();
+    print(latitude);
+    print(longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+      Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'),
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      print(data);
+    } else {
+      print('Error: ${response.statusCode}');
+    }
   }
 
   @override
